e3f80c86132f4a0ab98c861f959685241518b1e1
Reuben Stump <reuben.stump@servicenow.com>
2016-08-24 15:21:22 -0400

    added bug to unit tests WHT-1

--- tennis_unittest.py
+++ tennis_unittest.py
@@ -49,7 +49,7 @@ test_cases = [
     (6, 5, 'Advantage One', 'One', 'player2'),
     (5, 6, 'Advantage Two', 'player1', 'Two'), 
 
-#    (0, 0, 'Win for player2', 'player1', 'player2'),
+    (0, 0, 'Win for player2', 'player1', 'player2'),
     
     ]
 
